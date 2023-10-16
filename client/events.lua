7bd795475065690c5cb8e93dae74893a2438267a
BuildTools <unconfigured@null.spigotmc.org>
2023-10-16 12:36:41 +0200

    error correction

--- client/events.lua
+++ client/events.lua
@@ -1,15 +1,24 @@
+objs = {}
+peds = {}
+
 AddEventHandler("entityDamaged", function(victim, culprit, weapon, baseDamage)
-    if IsScriptEntity(victim, peds) then
-        local pedname = GetScriptEntity(victim, peds)
-        debug("Shooted "..pedname)
-        peds[pedname] = PedHit(pedname, victim)
+    local entity_type = GetEntityType(victim)
+    -- objs[victim] = {}
+    if entity_type == 1 and not IsPedAPlayer(victim) and IsPedAPlayer(culprit) and IsWeaponValid(weapon) then
+        debug("Shooted npc "..victim)
+        local v1, v2 = PedHit(victim)
+        objs[victim] = v1
+        peds[objs[victim]] = v2
+    elseif entity_type == 3 then
+        debug("Shooted obj "..victim)
+        PedHit(victim)
     end
 end)
 
 AddEventHandler("onResourceStop", function(resource)
     -- remove peds/npcs when the resource stops
     if resource == GetCurrentResourceName() then
-        for k, v in pairs(peds) do
+        for k, v in pairs(objs) do
             DeleteEntity(v)
         end
     end
--- client/functions.lua
+++ client/functions.lua
@@ -19,86 +19,55 @@ function GetRandomItemName(props)
     return nil
 end
 
-function PedHit(name, entity)
-    local prop = GetRandomItemName(Config.npcs[name].props)
-    local position = Config.npcs[name].position
-    local ped = Config.npcs[name].ped
-
-    if GetEntityType(entity) == 1 then
-        DeleteEntity(entity)
-        return SpawnObj(prop, vec3(position))
-    elseif GetEntityType(entity) == 3 then
-        DeleteEntity(entity)
-        return SpawnPed(ped, position)
-    end
-    -- return false
-end
-
 function SpawnPed(model, pos)
-    local pedHash = GetHashKey(model)
+    local pedHash = model
     local position = vec4(pos)
-    
+
     RequestModel(pedHash)
     while not HasModelLoaded(pedHash) do Citizen.Wait(1) end
 
     local npc = CreatePed(4, pedHash, position, true, true)
-
     SetEntityCoordsNoOffset(npc, vec3(position), true, true, true)
-    SetModelAsNoLongerNeeded(pedHash)
-    FreezeEntityPosition(npc, true)
-    SetBlockingOfNonTemporaryEvents(npc, true)
     
-    -- workaround because if ped set to invincible it will not trigger the "entityDamaged" event
-    SetPedArmour(npc, 999999)
-    SetPedHelmet(npc, true)
-
-    RequestAnimDict("anim@amb@nightclub@peds@")
-    while not HasAnimDictLoaded("anim@amb@nightclub@peds@") do Citizen.Wait(1) end
-    TaskPlayAnim(npc, "anim@amb@nightclub@peds@", "amb_world_human_stand_guard_male_base", 1.0, 1.0, -1, 1, 1.0, 0, 0, 0)
     return npc
 end
 
 function SpawnObj(model, pos)
-    local model = GetHashKey(model)
-    local position = vec3(pos)
+    if type(model) == "string" then
+        model = GetHashKey(model)
+    end
     
     RequestModel(model)
     while not HasModelLoaded(model) do Citizen.Wait(1) end
 
-    local obj = CreateObject(model, vec3(position), false, true, false)
+    local obj = CreateObject(model, vec3(pos), false, true, false)
 
     SetModelAsNoLongerNeeded(model)
     PlaceObjectOnGroundProperly(obj)
+    SetEntityHeading(entity, pos.w)
     FreezeEntityPosition(obj, true)
     return obj
 end
 
-function SpawnPeds()
-    local peds = {}
-    for k, v in pairs(Config.npcs) do
-        
-        npc = SpawnPed(v.ped, v.position)
-
-        debug(string.format("+ %s", k))
-        peds[k] = npc
-    end
-    return peds
-end
-
-function IsScriptEntity(entity, table)
+function IsScriptObj(obj, table)
     for k, v in pairs(table) do
-        if entity == v then
+        if obj == v then
             return true
         end
     end
     return false
 end
 
-function GetScriptEntity(entity, table)
-    for k, v in pairs(table) do
-        if v == entity then
-            return k
-        end
+function PedHit(entity)
+    local prop = GetRandomItemName(Config.props)
+    local position = vec4(GetEntityCoords(entity), GetEntityHeading(entity))
+    local ped_model = GetEntityModel(entity)
+    
+    if GetEntityType(entity) == 1 then
+        DeleteEntity(entity)
+        return SpawnObj(prop, position), ped_model
+    elseif GetEntityType(entity) == 3 and IsScriptObj(entity, objs) then
+        DeleteEntity(entity)
+        return SpawnPed(peds[entity], position), nil
     end
-    return nil
 end
